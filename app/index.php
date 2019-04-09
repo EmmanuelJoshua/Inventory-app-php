59e2a716387a5ac7293dd484dbdf008361d73044
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-04-09 15:14:30 +0100

    Update index.php
    Updated Skeleton HTML File.

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Palace Petroleum</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Website Icon -->
+	<link rel="icon" href="">
+	<!-- Website Stylesheet -->
+	<link href="../assets/css-app-files/test.css">
+	<!--Bootstrap -->
+	<link href="../assets/libraries/css-libs/bootstrap.css">
+</head>
+<body>
+	
+</body>
+<script type="text/javascript" src="../assets/js-app-files/test.js"></script>
+</html>
