eae24ff1cb2f4d30836c42a5cbda211606864bcb
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-04-09 16:09:33 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -6,11 +6,23 @@
 	<!-- Website Icon -->
 	<link rel="icon" href="">
 	<!-- Website Stylesheet -->
-	<link href="../assets/css-app-files/test.css">
+	<link rel="stylesheet" href="../app/assets/css-app-files/test.css">
 	<!--Bootstrap -->
-	<link href="../assets/libraries/css-libs/bootstrap.css">
+	<link rel="stylesheet" href="../app/assets/libraries/css-libs/bootstrap.css">
 </head>
 <body>
+	<header id="header">
+		<div class="col-md-6 mx-auto text-center text-white mt-5">
+            <h2>LOGIN</h2>
+            <div class="form-group">
+               <form method="post" onsubmit="submitForm(event)">
+	                <input class="form-control col-9 mx-auto" placeholder="Username" type="text" id="username">
+	                <input class="form-control col-9 mx-auto" placeholder="Password" type="password" id="password">
+	                <button class="btn btn-primary mt-3" type="submit" id="getData">Sign Up</button>
+            	</form>
+            </div>
+        </div>
+	</header>
 	
 </body>
 <script type="text/javascript" src="../assets/js-app-files/test.js"></script>
