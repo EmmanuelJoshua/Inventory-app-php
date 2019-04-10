7c614ee0e27b353b2b250d34218f668e9e0d3dd7
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-04-10 02:11:17 +0100

    More Changes.

--- app/index.php
+++ app/index.php
@@ -12,11 +12,27 @@
 	</head>
 	<body>
 		<header id="header">
-			<nav class="navbar-nav">
-				
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#">Palace Petroleum</a>
+					</div>
+					<div class="collapse navbar-collapse">
+
+                    </div>
+                    <div class="collapse navbar-collapse">
+                    	 <ul class="navbar-nav">
+                    	 	<li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="">
+                                About
+                            </a>
+                        </li>
+                    	 </ul>
+                    </div>
+				</div>
 			</nav>
 			<div class="container">
-				<div class="col-md-6 text-dark pt-4 pb-4 mt-5" id="login">
+				<div class="col-md-6 text-dark pt-5 pb-5" id="login">
 					<h2 class="display-4 text-center">LOGIN</h2>
 					<div class="container">
 						<form method="post" onsubmit="submitForm(event)">
@@ -24,19 +40,19 @@
 								<label class="form-label mx-auto h5" for="user[login]">Username</label>
 								<input class="form-control col-12  mb-4" type="text" id="username">
 							</div>
-
 							<div class="form-group">
 								<label class="form-label h5" for="user[login]">Password</label>
 								<input class="form-control col-12 mb-4" type="password" id="password">
 							</div>
-						
-						<input class="btn btn-primary text-center" type="submit" id="getData" value="Login">
-					</form>
+							<div class="text-center">
+								<input class="btn btn-primary" type="submit" id="getData" value="Login">
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	</header>
-	
-</body>
-<script type="text/javascript" src="../app/assets/js-app-files/login.js"></script>
+		</header>
+		
+	</body>
+	<script type="text/javascript" src="../app/assets/js-app-files/login.js"></script>
 </html>
