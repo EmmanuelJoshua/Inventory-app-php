f6e80baf76d64565ca88bbf3f86f04bb4c5ae0cc
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-04-10 03:31:50 +0100

    More Changes.

--- app/index.php
+++ app/index.php
@@ -9,9 +9,10 @@
 		<link rel="stylesheet" href="../app/assets/libraries/css-libs/bootstrap.css">
 		<!-- Website Stylesheet -->
 		<link rel="stylesheet" href="../app/assets/css-app-files/login.css">
+		<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
 	</head>
-	<body id="header">
-		<header>
+	<body >
+		<header id="header">
 			<nav class="navbar navbar-default">
 				<div class="container">
 					<ul class="nav navbar-nav">
@@ -44,11 +45,15 @@
 						</div>
 					</div>
 					<div class="col-md-6 text-white pt-5 pb-5 mt-3">
-						<h1 class="text-center">Palace Petroleum</h1>
+						<h1 class="text-center display-4" style="font-family: Pacifico">Palace Petroleum</h1>
 					</div>
 				</div>
 			</div>
 		</header>
+
+		<div id="footer" class="bg-dark text-white">
+			<span>© All Rights Reserved.</span>
+		</div>
 		
 	</body>
 	<script type="text/javascript" src="../app/assets/js-app-files/login.js"></script>
