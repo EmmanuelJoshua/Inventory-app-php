c02965464321194042d82e6b25dc184d7a5bf3d3
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-04-22 16:02:05 +0100

    Modals and some changes.

--- app/users.php
+++ app/users.php
@@ -23,7 +23,7 @@
 				<div class="row">
 					<div class="container-fluid">
 						<div class="links col-12 rounded text-white">
-							<a href="#" style="color: #8CC2FF">Dashboard</a><span> / Users</span>
+							<a href="dashboard.php" style="color: #8CC2FF">Home</a><span> / Users</span>
 						</div>
 						<div>
 						</div>
@@ -31,13 +31,87 @@
 							<div class="container-fluid">
 								<div class="mt-4 top-controls mb-4">
 									<div class="form-inline">
-										<button class="btn btn-primary mr-1">Add<i class="fas fa-plus-circle"></i></button>
 										<select class="form-control col-md-2 col-4">
 											<option>Search by</option>
 											<option>Name</option>
 											<option>Role</option>
 										</select>
 										<input type="text" name="" class="form-control ml-1 col-md-2 col-4" placeholder="">
+										<button class="btn btn-primary mr-1" data-toggle="modal" data-target="#addUser">Add<i class="fas fa-plus-circle"></i></button>
+										<div class="modal fade" id="addUser" tabindex="-1" role="dialog" aria-labelledby="addUserModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addUserModal">Add User <i class="fas fa-user-plus"></i></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+  <div class="form-group mb-3">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group mb-3">
+    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <div class="col-sm-10"> 
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Role:</label>
+    <div class="col-sm-10"> 
+      <input type="text" class="form-control" id="pwd" placeholder="Enter Role">
+    </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary pl-1 pr-1">Save<i class="fas fa-check-circle"></i></button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="updateUser" tabindex="-1" role="dialog" aria-labelledby="updateUserModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="updateUserModal">Update User <i class="fas fa-user-edit"></i></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+  <div class="form-group mb-3">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group mb-3">
+    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <div class="col-sm-10"> 
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Role:</label>
+    <div class="col-sm-10"> 
+      <input type="text" class="form-control" id="pwd" placeholder="Enter Role">
+    </div>
+  </div>
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary pl-1 pr-1">Save<i class="fas fa-check-circle"></i></button>
+      </div>
+    </div>
+  </div>
+</div>
 									</div>
 								</div>
 							</div>
@@ -47,10 +121,10 @@
 								<table class="table">
 									<thead>
 										<tr>
-											<th scope="col">Username</th>
+											<th scope="col">User Name</th>
 											<th scope="col">Role</th>
 											<th scope="col">Last Login</th>
-											<th scope="col">Actions</th>
+											<th scope="col">Options</th>
 										</tr>
 									</thead>
 									<tbody>
@@ -59,22 +133,24 @@
 											<td>Admin</td>
 											<td>Unknown</td>
 											<td><div class="nav-item dropdown show">
-                <button class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Actions
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="users.php" style="font-family: 'Titillium Web'">User Settings<i class="fas fa-user-cog ml-4"></i></a>
-                </div>
-              </div></td>
+												<button class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												Actions
+												</button>
+												<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+													<button class="dropdown-item" href="#" style="font-family: 'Titillium Web'" data-toggle="modal" data-target="#updateUser">Update<i class="fas fa-user-edit ml-4"></i></button>
+													<button class="dropdown-item" style="font-family: 'Titillium Web'">Delete<i class="fas fa-user-times ml-4"></i></button>
+												</div>
+											</div>
+										</td>
 										</tr>
-									</tbody>
-								</table>
-							</div>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
-			</body>
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		</html>
+			</div>
+		</body>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</html>
