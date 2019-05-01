25fde4ff82580c9c3706a49913c441696e6ce254
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-05-01 03:03:49 +0100

    Pump Page.

--- app/pump.php
+++ app/pump.php
@@ -18,8 +18,10 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	</head>
 	<body>
+		<!-- DASHBOARD WRAPPER -->
 		<div class="dashboard-wrapper pt-3">
 			<div class="container">
+				<!-- ROW CONTAINING LINK TO HOME/DASHBOARD -->
 				<div class="row">
 					<div class="container-fluid">
 						<div class="links col-12 rounded text-white mt-3">
@@ -27,7 +29,295 @@
 						</div>
 					</div>
 				</div>
+				<!-- TOP CONTROLS (BUTTONS and DROPDOWNS) -->
+				<div class="row">
+					<div class="container-fluid">
+						<div class="mt-4 top-controls mb-4">
+							<button class="btn btn-primary mr-1" data-toggle="modal" data-target="#registerPump" style="float: right; width: 145px !important;">Register Pump<i class="fas fa-plus-circle"></i></button>
+							<div class="form-inline">
+								<select class="custom-select form-control col-md-2 col-5">
+									<option>Search by</option>
+								</select>
+								<input type="text" name="" class="form-control ml-1 col-md-2 col-5" placeholder="">
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- PUMP SALES/PUMP METER BUTTON -->
+				<div class="row">
+					<div class="container-fluid text-center">
+						<div class="top-controls mb-3">
+							<button class="btn btn-primary mr-1" style="width: 200px !important;" data-toggle="modal" data-target="#pumpSales">Pump Sales/Pump Meter</button>
+						</div>
+					</div>
+				</div>
+				<!-- DIV CONTAINING MODALS -->
+				<div class="top-controls">
+					<!-- REGISTER PUMP MODAL -->
+					<div class="modal fade" id="registerPump" tabindex="-1" role="dialog" aria-labelledby="registerPumpModal" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="registerPumpModal"><i class="fas fa-plus-square"></i></h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<form class="form-horizontal">
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control" placeholder="Enter Pump Name" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Opening Meter Number" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Product Type" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Product Description" style="width: 100%;">
+											</div>
+										</div>
+										<h5 class="productTitle">Pump Meter Margin In %</h5>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Actual" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Pump Meter" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Margin In %" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<select class="custom-select form-control">
+													<option>Select Sources Of Product</option>
+												</select>
+											</div>
+										</div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary pl-1 pr-1" data-dismiss="modal" style="width: 80px !important;">Cancel<i class="fas fa-times-circle"></i></button>
+									<button type="button" class="btn btn-primary pl-1 pr-1">Save<i class="fas fa-check-circle"></i></button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- PUMP SALES MODAL -->
+					<div class="modal fade" id="pumpSales" tabindex="-1" role="dialog" aria-labelledby="pumpSalesModal" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="pumpSalesModal"><i class="fas fa-plus-square"></i></h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<form class="form-horizontal">
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="date" class="form-control">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Pump Name" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Closing Meter" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Attendant's Name" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Received From Attendants" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Product" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Source Tank" style="width: 100%;">
+											</div>
+										</div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary pl-1 pr-1" data-dismiss="modal" style="width: 80px !important;">Cancel<i class="fas fa-times-circle"></i></button>
+									<button type="button" class="btn btn-primary pl-1 pr-1">Save<i class="fas fa-check-circle"></i></button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- EDIT PUMP MODAL -->
+					<div class="modal fade" id="editPump" tabindex="-1" role="dialog" aria-labelledby="editPumpModal" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="editPumpModal"><i class="fas fa-user-edit"></i></h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<form class="form-horizontal">
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control" placeholder="Enter Pump Name" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Opening Meter Number" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Product Type" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Product Description" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Actual" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Enter Pump Meter" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<input type="text" class="form-control"placeholder="Margin In %" style="width: 100%;">
+											</div>
+										</div>
+										<div class="form-group mb-3">
+											<div class="col-sm-12">
+												<select class="custom-select form-control">
+													<option>Select Sources Of Product</option>
+												</select>
+											</div>
+										</div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary pl-1 pr-1" data-dismiss="modal" style="width: 80px !important;">Cancel<i class="fas fa-times-circle"></i></button>
+									<button type="button" class="btn btn-primary pl-1 pr-1" style="width: 90px !important;">Update<i class="fas fa-check-circle"></i></button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- DELETE PUMP MODAL -->
+					<div class="modal fade" id="deletePump" tabindex="-1" role="dialog" aria-labelledby="deletePumpModal" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="deletePumpModal"><i class="fas fa-trash-alt"></i></h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<h4>Do you want to delete this pump?</h4>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary" data-dismiss="modal" style="width: 90px !important;">Cancel<i class="fas fa-times-circle"></i></button>
+									<button type="button" class="btn btn-primary pl-1 pr-1">Yes<i class="fas fa-check-circle"></i></button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- CONTAINER FOR TABLES -->
+				<div class="row">
+					<div class="container-fluid">
+						<!-- DIV FOR PRODUCT -->
+						<div id="product" class="">
+							<div class="table-responsive">
+								<table class="table bg-white" style="font-size: 0.9em;">
+									<thead>
+										<tr>
+											<th scope="col">Date</th>
+											<th scope="col">Product</th>
+											<th scope="col">Source Tank</th>
+											<th scope="col">Pump Name</th>
+											<th scope="col">Opening Meter</th>
+											<th scope="col">Closing Meter</th>
+											<th scope="col">Attendent Name</th>
+											<th scope="col">Expectant Sales</th>
+											<th scope="col">Rate</th>
+											<th scope="col">Expected</th>
+											<th scope="col">Received</th>
+											<th scope="col">Difference</th>
+											<th scope="col">Status</th>
+											<th scope="col">Options</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td><div class="nav-item dropdown show">
+												<button class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												Actions
+												</button>
+												<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+													<button class="dropdown-item" href="#" style="font-family: 'Titillium Web'" data-toggle="modal" data-target="#editPump">Edit<i class="fas fa-edit ml-4"></i></button>
+													<button class="dropdown-item" style="font-family: 'Titillium Web'" data-toggle="modal" data-target="#deletePump">Delete<i class="fas fa-trash-alt ml-4"></i></button>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	</body>
-	</html>
+	</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
