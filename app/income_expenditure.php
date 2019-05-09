e4083f01d1329b839ea4318cf7df062dabbf94de
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-05-09 01:47:39 +0100

    Income.

--- app/income_expenditure.php
+++ app/income_expenditure.php
@@ -18,8 +18,10 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	</head>
 	<body>
+		<!-- DASHBOARD WRAPPER -->
 		<div class="dashboard-wrapper pt-3">
 			<div class="container">
+				<!-- ROW CONTAINING LINK TO HOME/DASHBOARD -->
 				<div class="row">
 					<div class="container-fluid">
 						<div class="links col-12 rounded text-white mt-3">
@@ -27,7 +29,49 @@
 						</div>
 					</div>
 				</div>
+				<!-- CONTAINER FOR TABLES -->
+				<div class="row">
+					<div class="container-fluid">
+						<!-- DIV FOR INCOME/EXPENDITURE -->
+						<div id="" class="">
+							<div class="top-controls mt-3">
+								<div class="form-inline">
+									<select class="custom-select form-control col-md-2 col-5">
+										<option>Search by</option>
+									</select>
+									<input type="text" name="" class="form-control ml-1 col-md-2 col-5" placeholder="">
+								</div>
+							</div>
+							<div class="table-responsive">
+								<table class="table bg-white mt-4">
+									<thead>
+										<tr>
+											<th scope="col">Date</th>
+											<th scope="col">Particulars</th>
+											<th scope="col">Description</th>
+											<th scope="col">Ref</th>
+											<th scope="col">Income</th>
+											<th scope="col">Expenditure</th>
+											<th scope="col">Balance</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+											<td>Unknown</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
-	</html>
+</html>
