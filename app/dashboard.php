5c10c7888dc6738505a05df75044c29b3f3ebe3a
Emmanuel Joshua <40811019+EmmanuelJoshua@users.noreply.github.com>
2019-05-12 21:41:25 +0100

    Dashboard.

--- app/dashboard.php
+++ app/dashboard.php
@@ -20,32 +20,44 @@
 	</head>
 	<body>
 		<div class="dashboard-wrapper">
-				<div class="dash-display mb-3">
-					<h1>Dashboard</h1>
+			<div class="dash-display mb-3">
+				<h1>Dashboard</h1>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="dash-content col-md-11 mx-auto text-center">
+						<h2 style="font-family: pacifico; font-size: 40px;">Palace Petroleum</h2>
+					</div>
 				</div>
-
+			</div>
+			<div class="container">
+				<div class="row mb-4">
+					<div class="col-md-5 mx-auto dash-content text-center mt-3">
+						<img src="../app/assets/images/user.png" width="170">
+						<hr>
+						<span>Mr. User</span>
+						
+					</div>
+					<div class="col-md-5 mx-auto dash-content text-center mt-3 time">
+						<i class="far fa-clock"></i><span>2019-02-02</span>
+						<hr>
+						<i class="fas fa-history"></i><span>8:54 PM</span>
+						
+					</div>
+				</div>
+			</div>
+			<div id="footer">
 				<div class="container">
-					<div class="row mb-4">
-						<div class="col-5 mx-auto dash-content text-center">
-							<span>Mr. User</span>
-							
-						</div>
-
-						<div class="col-5 mx-auto dash-content text-center">
-							<span>2019-02-02</span>
-							
+					<div class="row justify-content-center">
+						<div class="col-md-6 col-12">
+							<p style="text-align: center;">
+								&copy;
+								<script>document.write(new Date().getFullYear());</script>, Palace Petroleum
+							</p>
 						</div>
 					</div>
 				</div>
-
-				<div class="container">
-					<div class="row mb-3">
-						<div class="dash-content col-7 mx-auto text-center">
-							<h2 style="font-family: pacifico">Palace Petroleum</h2>
-						</div>		
-					</div>
-				</div>
-
+			</div>
 		</div>
 	</body>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
